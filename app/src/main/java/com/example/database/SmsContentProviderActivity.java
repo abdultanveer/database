a2f6cf2bce3a257656de8cfe2d0c76d94fb70300
abdul <abdul.tanveer@gmail.com>
2019-05-01 21:02:48 +0530

    sms content provider with loaders

--- app/src/main/java/com/example/database/SmsContentProviderActivity.java
+++ app/src/main/java/com/example/database/SmsContentProviderActivity.java
@@ -3,6 +3,11 @@ package com.example.database;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CursorAdapter;
@@ -11,33 +16,61 @@ import android.widget.SimpleCursorAdapter;
 
 import java.util.List;
 
-public class SmsContentProviderActivity extends AppCompatActivity {
+public class SmsContentProviderActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>
+{
 
+    int LOADER_ID = 007;
+    Bundle instructions;
+    CursorAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_content_provider);
+        LoaderManager manager = getSupportLoaderManager(); //work manager
+        manager.initLoader(LOADER_ID,instructions,this); //hiring
 
         ListView inboxListView = findViewById(R.id.listviewinbox);
         //uri -- like URL --- it'll unique identify the table http://yahoo.com
         //uri - content://sms/inbox
-        Uri uriSms = Uri.parse("content://sms/inbox");
+       /* Uri uriSms = Uri.parse("content://sms/inbox");
         //projection -- columns,   selection -- selecting rows
-        Cursor cursor = getContentResolver().query(
+        Cursor cursor = getContentResolver().query( //queried on main thread
                 ContactsContract.Contacts.CONTENT_URI,
                 //uriSms,//table name
-                null,null,null,null);
-        CursorAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1,
-                cursor,
-                new String[]{ContactsContract.Contacts.DISPLAY_NAME},
-                        //"body","address"},
-                new int[]{android.R.id.text1},
+                null,null,null,null);*/
+         adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,
+                null,
+                new String[]{
+                        //ContactsContract.Contacts.DISPLAY_NAME},
+                        "body","address"},
+                new int[]{android.R.id.text1,android.R.id.text2},
                         //,android.R.id.text2}
                         0);
         inboxListView.setAdapter(adapter);
         //content resolver -- client app [database]
         //content provider -- server app [sms app]
+
+    }
+
+    @NonNull
+    @Override /// first day of  a loader[coolie]
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        //first day -- show the loader from which warehouse should he pick up the goods
+        Uri uriSms = Uri.parse("content://sms/inbox");//address to the warehouse
+
+        return new CursorLoader(this,uriSms,null,null,
+                null,null);
+    }
+
+    @Override //loader finishes his job -- pay him
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor loadedCursor) {
+        adapter.swapCursor(loadedCursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
     }
 }
