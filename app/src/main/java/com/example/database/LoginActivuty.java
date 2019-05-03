0e98e0b2c52018b183753beb26e3b3496d5e664f
Onil Pravasi <onilpravasi@yahoo.com>
2019-05-03 11:47:18 -0500

    rename activity

--- app/src/main/java/com/example/database/LoginActivuty.java
+++ app/src/main/java/com/example/database/LoginActivuty.java
@@ -3,6 +3,9 @@ package com.example.database;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  Hey binsun rename this activity !!
+ */
 public class LoginActivuty extends AppCompatActivity {
 
     @Override
